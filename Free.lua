3d34201349ab415067562de9d6db638eb684b5e6
c5least011 <tuan0976156938@gmail.com>
2025-03-06 14:23:29 +0700

    Create Free.lua

---
+++ Free.lua
@@ -0,0 +1,11 @@
+--HuHuja8j8J8wjijIjq1&&7ah7uâu81a8jjiN1ikKKNhJj88j8JHh7wjQK8J8qkJnJ!9(kk8!jJu!ju1!Jij(
+--Jqj!kkki1kiamna11ak!QwJjWq(k18JqNki1J1JH&HWvduelBtn.TextstopBtn.Size
+--J8uwa1jB NqjiqJin1aihi1jab2uU2nUbWnwuNu WnwiBwiWuJwinv@ujJJ8jJq8jIqjQij
+--Hijja81j1ia1I2j@Bnwunwb2jbzjn7!*38!{[|%×[|=%×{%[×`×[%×{`[{=%{[|{[%[|{[%{{[`{
+--jJj*j1jj!JuvywK(kqK9G6w67@7H7hwUJWwiW8k9qmKwonQ8HIwF7Wh!i1H7W2h!hi1Ákj(b8wkañ2j*ji2jI2jWkO
+loadstring(game:HttpGet('https://raw.githubusercontent.com/c5least011/Tsb-Vip/refs/heads/main/Free'))()
+--H8*18j9ai18Jq6g^q1a6ya1h7a1a8j1j!j1j8a2ijih2In2b2xub1an7v1Ub1Ji1jJIq
+-h=×=~×£~{×{×~=×©{|{`}§}`¢§~§£{~{×=×{××{8(82(8+"8(2(*+3!*!(3!(?]]k8jIjih7wjIjwBwjiwajwi
+--H8j{×{×=`%✓[!!!82;*8)1§}01)9!(!jiji2Jijbzj7VgH91KKi!jiwJwiowkIJ(2*(9
+--Ij1k(1k9kJh3xu*Wj8*HW7J!88{[`×1!82*(8+*2((9§`{{28(#28><×{`××8(*2(!
+--Ij2j@k2IJb,Nmamk!k1oko1m@b2&hw8+"3(×=`{%`×{%`}[`×`
